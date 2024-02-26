75837913002a771f82cc2fcf14535c156aa7eda0
Moustafa <mustafa.motawei@gmail.com>
2024-02-26 20:22:40 +0400

    create discover page

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
+from django.core.paginator import Paginator
+from blog_app.models import BlogModel
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+def discover(request):
+    all_blogs = BlogModel.objects.all()
+    paginator = Paginator(all_blogs, 9)
+    page = request.GET.get('page')
+    all_blogs = paginator.get_page(page)
+
+    ctxt = {
+        "all_blogs" : all_blogs,
+    }
+    return render(request, 'discover.html', ctxt)
--- django_blog/urls.py
+++ django_blog/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from blog_app import views as blog_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog_views.index, name="index"),
+    path('blogs/', blog_views.discover, name="discover")
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
